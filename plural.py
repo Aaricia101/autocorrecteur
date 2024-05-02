6f8833b8f693dddcbbbbda83b21bba388c5323f5
Jerome <47067008+RNGPuns@users.noreply.github.com>
2024-05-02 16:59:42 -0400

    J'essaie des choses :/

--- plural.py
+++ plural.py
@@ -3,19 +3,35 @@ import random
 from nltk.corpus import words
 from nltk.tokenize import word_tokenize
 from nltk.tag import pos_tag
+import re
 
-def plural(word):
-     return {'last_letter': word[-1]}
 
 #Va chercher les données du dataset
 list = words.words()
-#print(list[:100])
+#print(list[:10])
 #random.shuffle(list)
+
+
+import inflect
+engine = inflect.engine()
+
 
 for w in list[:10]:
     w1 = word_tokenize(w)
     tagged_words = pos_tag(w1)
-    print(tagged_words)
+    #print(tagged_words)
 
+mots = []
+with open('auto2.txt', 'r', encoding='utf-8') as f:
+    for i in range(22323):
+        next(f)
+    file_name_data = f.read().strip('\n').replace('\n', ' ').split('.')
+    for x in file_name_data:
+         for w in x.split(" "):
+                    for word, pos in pos_tag(word_tokenize(w)):
+                         if pos == 'VBZ':
+                              print(word)
+          
     
-    print(plural(w))
+
+#print(mots[:25])
