79ab97fd9952f07d5c6197e5003f31c98a9f5f37
AxeBurningfire <45503146+AxeBurningfire@users.noreply.github.com>
2024-05-26 01:38:35 -0400

    Upd8 Part 2

--- BrowserExt/server/index.js
+++ BrowserExt/server/index.js
@@ -8,13 +8,14 @@ app.use(cors())
 
 const spawner = require('child_process').spawn;
 app.post('/autocorrectext', (req, res) => {
-    var textToCorrect = req.body.textToCorrect.replace('\n', '');
+    var textToCorrect = req.body.textToCorrect.replace('\n', '').replaceAll('’', '\'');
 
     const python_process = spawner('py', [__dirname + '/algo.py', textToCorrect])
     console.log("data sent to python: " + textToCorrect);
 
     python_process.stdout.on('data', (data) => {
         let dataArr = data.toString().split('\n');
+        console.log("data recieved from python: " + data.toString())
         let ufData = [];
         let wordData = [];
         for(let i = 0; i < dataArr.length; i++) {
@@ -27,9 +28,23 @@ app.post('/autocorrectext', (req, res) => {
             }
         }
 
-        console.log("data recieved from python: " + ufData)
-        res.send(ufData);
+        try {
+            res.send(ufData);
+        }
+        catch(e) {}
     });    
+});
+
+app.post('/ajoutmot', (req, res) => {
+    console.log("----------------------------")
+    console.log("- " + req.body.ajoutMot + " -")
+    console.log("----------------------------")
+    var nouvMot = req.body.ajoutMot.toString().replace('\n', '').replaceAll('’', '\'');
+
+    fs.appendFile("newWords.txt", "\n" + nouvMot, function(err, result) {
+        //if(err) { console.log('error', err); }
+      }
+    );
 });
 
 const port = process.env.PORT || 8080;
